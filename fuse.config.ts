3f52d92570bb532844dfb2511aa0ea47d627ae20
Mishal-SecOps24 <mmishal@secops24.com>
2026-09-10 19:11:16 +0530

    Remove fuse.config.ts for demo reset

--- fuse.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@fuse/sdk'
-
-export default defineConfig({
-  name: 'Fuse Test',
-  port: 3000,
-  memory: 512,
-  cpu: 256,
-  security: { dataSensitivity: 'Internal', audience: [] },
-})
